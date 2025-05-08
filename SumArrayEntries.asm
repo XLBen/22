ccfc5e8bcf683eb79a8bfc1c239af5bd47ee3240
xiaoliu ben <vvsp1469@leeds.ac.uk>
2025-05-08 14:56:12 +0000

    main

--- SumArrayEntries.asm
+++ SumArrayEntries.asm
@@ -1,40 +1,46 @@
-@R2
-M=0
+@R2 // Read the address of R2
+M=0 // R2 = 0 (initialize accumulator)
 
-@R1
-D=M
-@END
-D;JLE
+@R1 // Read the address of R1
+D=M // D = value of R1 (array length)
+@END // Prepare to jump to END
+D;JLE // If D <= 0, end directly (invalid array length)
 
-@R3
-M=0
-@R0
-D=M
-@R4
-M=D
+// Initialize loop variables
+@R3 // Read the address of R3
+M=0 // Clear loop counter
+@R0 // Read the address of R0
+D=M // Array start address
+@R4 // Read the address of R4
+M=D // Save current pointer position
 
+// Main loop
 (LOOP)
-@R3
-D=M
-@R1
-D=D-M
-@END_LOOP
-D;JEQ
+@R3 // Read the address of R3
+D=M // D = current loop count
+@R1 // Read the address of R1
+D=D-M // D = count - array length
+@END_LOOP // Prepare to jump to END_LOOP
+D;JEQ // If count == Array length, end loop
 
-@R4
-A=M
-D=M
-@R2
-M=D+M
+// Accumulate
+@R4 // Read current pointer
+A=M // A = current pointer value
+D=M // D = current array element value
+@R2 // Read address of R2
+M=D+M // R2 += current element value
 
-@R4
-M=M+1
-@R3
-M=M+1
-@LOOP
-0;JMP
+// Update pointer and counter
+@R4 // Read address of R4
+M=M+1 // Move to next element
+@R3 // Read address of R3
+M=M+1 // Loop count++
+@LOOP // Prepare to jump back to LOOP
+0;JMP // Continue loop unconditionally
 
+// End of loop
 (END_LOOP)
+// Program terminates
 (END)
-@END
+@END // Read address of END
 0;JMP
